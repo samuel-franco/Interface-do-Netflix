a922c9c739b5b8581d74aa37d8bfecb5c51ec2dc
samuel-franco <samuelfrako66@gmail.com>
2022-12-15 22:10:03 -0300

    commit inicial do projeto

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "02e07736318335b890435d9b12cfc3ff";
+
+let img_url = "https://image.tmdb.org/t/p/w500/";
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
